d90859ffffb728c213a53cb5aa1f504f7f7e0aa8
Joe Fabisevich <Joe@fabisevi.ch>
2019-08-22 18:11:33 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+
+// @import "index.css";
+app.use(express.static('public'))
+
+app.get('/', function(req, res) {
+  let state = process.env.BUSTED_STATE || "Maybe"
+  res.send(
+  	stylizedString(state)
+  );
+});
+
+app.get('/set', function(req, res) {
+    let token = req.query.token;
+
+    if (token == null) {
+    	res.send('Nothing to see here. 🚷');
+    	return;
+    }
+
+    if (token !== process.env.AUTH_TOKEN) {
+    	res.send(
+		  stylizedString("Nice try!")
+    	);
+    	return;
+    }
+
+    let value = req.query.value;
+
+    if (value.toLowerCase() === "yes") {
+    	res.send(
+		  stylizedString("Yes! ✌🏼")
+    	);
+    	process.env["BUSTED_STATE"] = "Yes"
+		// set cache values
+    } else {
+    	res.send(
+		  stylizedString("No! 🙅🏻‍♀️")
+    	);
+
+    	process.env["BUSTED_STATE"] = "No"
+    	// set cache values
+    }
+});
+
+function stylizedString(value) {
+	return `
+		<div class="centered">${value}</div>
+	`;
+}
+
+// Start script
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Hello world listening on port', port);
+});
